7e12ac980e99ea7b4ffd5060e1426b2225461c45
prathyushabhumireddy <prathyushabhumireddy26@gmail.com>
2021-12-28 20:41:13 +0530

    assignment

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,12 +1,26 @@
 function getNameFromCommandLine() {
+    let name = process.argv
+    len = name.length
+    return(name[len-1])
+
+
     // Write you code here, name should be taken as args in process.argv
 }
 
 function getNameFromEnv() {
+    envName=process.env.name
+    return envName
     // Write your code here
 }
 
 function getNameFromReadLine() {
+    var readLine = require("readline");
+    var rl = readLine.createInterface(
+        process.stdin,process.stdout);
+        rl.question(" ",(age)=>{
+            console.log(age);
+            rl.close();
+        });
     // Write your code here
 }
 
